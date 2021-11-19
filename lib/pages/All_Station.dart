25dd17c24a1a6cab950b7ff7dcfb56deef39cf0d
AniketDubey <aniket.dubey0206@gmail.com>
2021-11-20 00:09:30 +0530

    Updated

--- lib/pages/All_Station.dart
+++ lib/pages/All_Station.dart
@@ -1,12 +1,25 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:minoragain/models/DUMMYDATA.dart';
 
 class AllStation extends StatelessWidget {
   const AllStation({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("All Stations"),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: ListTile(),
+          );
+        },
+        itemCount: sList.length,
+      ),
+    );
   }
 }
