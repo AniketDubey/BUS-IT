e4e3dd1af49fa0c73317c903d817836bb6b5291e
AniketDubey <aniket.dubey0206@gmail.com>
2021-11-20 12:27:37 +0530

    Updated

--- lib/pages/BusDetailScreen.dart
+++ lib/pages/BusDetailScreen.dart
@@ -45,111 +45,123 @@ class _BusDetailScreenState extends State<BusDetailScreen> {
         appBar: AppBar(
           title: Text("Details"),
         ),
-        body: Consumer<BList>(
-          builder: (ctx, data, ch) {
-            return _isLoading
-                ? Center(
-                    child: CircularProgressIndicator(
-                      strokeWidth: 6,
-                    ),
-                  )
-                : data.l4.length == 0
-                    ? AlertDialog(
-                        scrollable: true,
-                        title: Text("Oops !"),
-                        content: Text("No bus for the route"),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: Text("OK"),
-                          )
-                        ],
+        body: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.asset(
+                "assets/b1.png",
+                fit: BoxFit.fill,
+              ),
+            ),
+            Consumer<BList>(
+              builder: (ctx, data, ch) {
+                return _isLoading
+                    ? Center(
+                        child: CircularProgressIndicator(
+                          strokeWidth: 6,
+                        ),
                       )
-                    : ListView.builder(
-                        itemBuilder: (ctx, index) {
-                          Map<String, dynamic> temp = data.l4[index];
-                          Map<String, dynamic> mp = temp["Sdetails"];
+                    : data.l4.length == 0
+                        ? AlertDialog(
+                            scrollable: true,
+                            title: Text("Oops !"),
+                            content: Text("No bus for the route"),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text("OK"),
+                              )
+                            ],
+                          )
+                        : ListView.builder(
+                            itemBuilder: (ctx, index) {
+                              Map<String, dynamic> temp = data.l4[index];
+                              Map<String, dynamic> mp = temp["Sdetails"];
 
-                          return Padding(
-                            padding: EdgeInsets.only(
-                              top: 10,
-                              left: 10,
-                              right: 10,
-                            ),
-                            child: Card(
-                              child: ListTile(
-                                leading: Icon(Icons.train),
-                                title: Text(temp["BusNum"]),
-                                subtitle: Text(
-                                    "Available Seats ${100 - temp["PasLog"]}"),
-                                trailing: Text("Time Required"),
-                                onTap: () {
-                                  showDialog(
-                                    context: ctx,
-                                    builder: (cc) {
-                                      return AlertDialog(
-                                        //scrollable: true,
-                                        title: Text("Station Information"),
-                                        content: Padding(
-                                          padding: EdgeInsets.all(10),
-                                          child: Column(
-                                            children: [
-                                              Row(
+                              return Padding(
+                                padding: EdgeInsets.only(
+                                  top: 10,
+                                  left: 10,
+                                  right: 10,
+                                ),
+                                child: Card(
+                                  child: ListTile(
+                                    leading: Icon(Icons.train),
+                                    title: Text(temp["BusNum"]),
+                                    subtitle: Text(
+                                        "Available Seats ${100 - temp["PasLog"]}"),
+                                    trailing: Text("Time Required"),
+                                    onTap: () {
+                                      showDialog(
+                                        context: ctx,
+                                        builder: (cc) {
+                                          return AlertDialog(
+                                            //scrollable: true,
+                                            title: Text("Station Information"),
+                                            content: Padding(
+                                              padding: EdgeInsets.all(10),
+                                              child: Column(
                                                 children: [
-                                                  Text("Station Name"),
-                                                  Spacer(),
-                                                  Text("ETA"),
+                                                  Row(
+                                                    children: [
+                                                      Text("Station Name"),
+                                                      Spacer(),
+                                                      Text("ETA"),
+                                                    ],
+                                                  ),
+                                                  SizedBox(
+                                                    height: 15,
+                                                  ),
+                                                  ListView.builder(
+                                                    shrinkWrap: true,
+                                                    itemBuilder: (cctx, ind) {
+                                                      Map<String, dynamic>
+                                                          sName = mp[(ind + 1)
+                                                              .toString()];
+
+                                                      var name =
+                                                          sName.keys.toString();
+                                                      name = name.substring(
+                                                          1, name.length - 1);
+
+                                                      return Row(
+                                                        children: [
+                                                          Text("$name"),
+                                                          Spacer(),
+                                                          Text(
+                                                              "${sName[name][0]}"),
+                                                        ],
+                                                      );
+                                                    },
+                                                    itemCount: mp.length,
+                                                  ),
                                                 ],
                                               ),
-                                              SizedBox(
-                                                height: 15,
-                                              ),
-                                              ListView.builder(
-                                                shrinkWrap: true,
-                                                itemBuilder: (cctx, ind) {
-                                                  Map<String, dynamic> sName =
-                                                      mp[(ind + 1).toString()];
-
-                                                  var name =
-                                                      sName.keys.toString();
-                                                  name = name.substring(
-                                                      1, name.length - 1);
-
-                                                  return Row(
-                                                    children: [
-                                                      Text("$name"),
-                                                      Spacer(),
-                                                      Text("${sName[name][0]}"),
-                                                    ],
-                                                  );
-                                                },
-                                                itemCount: mp.length,
-                                              ),
+                                            ),
+                                            actions: [
+                                              TextButton(
+                                                  onPressed: () {
+                                                    Navigator.of(context,
+                                                            rootNavigator: true)
+                                                        .pop();
+                                                  },
+                                                  child: Text("OK"))
                                             ],
-                                          ),
-                                        ),
-                                        actions: [
-                                          TextButton(
-                                              onPressed: () {
-                                                Navigator.of(context,
-                                                        rootNavigator: true)
-                                                    .pop();
-                                              },
-                                              child: Text("OK"))
-                                        ],
+                                          );
+                                        },
                                       );
                                     },
-                                  );
-                                },
-                              ),
-                            ),
+                                  ),
+                                ),
+                              );
+                            },
+                            itemCount: data.l4.length,
                           );
-                        },
-                        itemCount: data.l4.length,
-                      );
-          },
+              },
+            ),
+          ],
         ),
       ),
     );
